f384b9f33f3f7ad965a25a6893e4ee4563c88689
Gena E <genaevstratov@yandex-team.ru>
2020-02-20 18:41:34 +0300

    dartfmt over the code

--- lib/flutter_gauge.dart
+++ lib/flutter_gauge.dart
@@ -1,54 +1,31 @@
 library flutter_gauge;
+
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_gauge/fluttergauge.dart';
 import 'package:rxdart/rxdart.dart';
 
-enum SecondsMarker {
-  minutes,
-  secondsAndMinute,
-  all,
-  none,
-  seconds
-}
+enum SecondsMarker { minutes, secondsAndMinute, all, none, seconds }
 
 enum Number {
   all,
   endAndStart,
   endAndCenterAndStart,
   none,
-
 }
 
-enum NumberInAndOut {
-  inside,
-  outside
-}
+enum NumberInAndOut { inside, outside }
 
-
-enum CounterAlign{
+enum CounterAlign {
   none,
   center,
   top,
   bottom,
 }
 
+enum Hand { none, long, short }
 
-enum Hand{
-  none,
-  long,
-  short
-}
-
-
-enum Animate{
-  none,
-  knock,
-  forget
-}
-
-
-
+enum Animate { none, knock, forget }
 
 class FlutterGauge extends StatefulWidget {
   int start;
@@ -77,24 +54,45 @@ class FlutterGauge extends StatefulWidget {
   TextStyle counterStyle;
   TextStyle textStyle;
 
-  FlutterGauge({this.isDecimal=true,this.inactiveColor = Colors.black,this.activeColor = Colors.red,this.textStyle,this.counterStyle,this.numberInAndOut = NumberInAndOut.inside,this.width/*,this.animate=Animate.none*/,this.paddingHand=30.0,this.circleColor = Colors.cyan,this.handColor = Colors.black,this.backgroundColor = Colors.cyan,this.indicatorColor = Colors.black,this.shadowHand=4.0,this.counterAlign=CounterAlign.bottom,this.number=Number.all,this.isCircle=true,this.hand= Hand.long,this.secondsMarker=SecondsMarker.all,this.handSize=30,this.start=0,this.end=100,@required this.index,this.fontFamily="",this.widthCircle=20,});
-
+  FlutterGauge({
+    this.isDecimal = true,
+    this.inactiveColor = Colors.black,
+    this.activeColor = Colors.red,
+    this.textStyle,
+    this.counterStyle,
+    this.numberInAndOut = NumberInAndOut.inside,
+    this.width /*,this.animate=Animate.none*/,
+    this.paddingHand = 30.0,
+    this.circleColor = Colors.cyan,
+    this.handColor = Colors.black,
+    this.backgroundColor = Colors.cyan,
+    this.indicatorColor = Colors.black,
+    this.shadowHand = 4.0,
+    this.counterAlign = CounterAlign.bottom,
+    this.number = Number.all,
+    this.isCircle = true,
+    this.hand = Hand.long,
+    this.secondsMarker = SecondsMarker.all,
+    this.handSize = 30,
+    this.start = 0,
+    this.end = 100,
+    @required this.index,
+    this.fontFamily = "",
+    this.widthCircle = 20,
+  });
 
   @override
   _FlutterGaugeState createState() => _FlutterGaugeState();
 }
 
 class _FlutterGaugeState extends State<FlutterGauge> {
-
-
   @override
   void initState() {
     super.initState();
-    Timer(Duration(milliseconds: 400), (){
+    Timer(Duration(milliseconds: 400), () {
       eventObservable.add(widget.index);
     });
   }
-
 
   PublishSubject<double> eventObservable = new PublishSubject();
 //  animateHourHand(index,oldIndex){
@@ -160,8 +158,6 @@ class _FlutterGaugeState extends State<FlutterGauge> {
 //
 //  }
 
-
-
   @override
   Widget build(BuildContext context) {
 //    animateHourHand(widget.index,7.0);
@@ -175,46 +171,35 @@ class _FlutterGaugeState extends State<FlutterGauge> {
             counterStyle: widget.counterStyle,
             textStyle: widget.textStyle,
             numberInAndOut: widget.numberInAndOut,
-            paddingHand : widget.paddingHand,
-            circleColor : widget.circleColor,
-            handColor : widget.handColor,
-            backgroundColor : widget.backgroundColor,
-            indicatorColor : widget.indicatorColor ,
-            shadowHand : widget.shadowHand,
-            counterAlign : widget.counterAlign,
-            number : widget.number,
-            isCircle : widget.isCircle,
-            hand : widget.hand,
-            secondsMarker : widget.secondsMarker,
-            handSize : widget.handSize,
-            start : widget.start,
-            end : widget.end,
-            highlightStart : (widget.end/widget.end.toInt()),
-            highlightEnd : (0/widget.end.toInt()),
-            eventObservable : eventObservable,
-            fontFamily : widget.fontFamily,
-            widthCircle : widget.widthCircle > 30 ? 30 :widget.widthCircle,
-            inactiveColor : widget.inactiveColor,
-            activeColor : widget.activeColor,
-            width : (
-                widget.width == null
-                    ?MediaQuery.of(context).size.width/2
-                    :widget.width < (MediaQuery.of(context).size.width/2)
-                    ?(MediaQuery.of(context).size.width/2)
-                    :(
-                    widget.width > (MediaQuery.of(context).size.width)
-                        ?MediaQuery.of(context).size.width
-                        :widget.width
-                )
-            )
-        )
+            paddingHand: widget.paddingHand,
+            circleColor: widget.circleColor,
+            handColor: widget.handColor,
+            backgroundColor: widget.backgroundColor,
+            indicatorColor: widget.indicatorColor,
+            shadowHand: widget.shadowHand,
+            counterAlign: widget.counterAlign,
+            number: widget.number,
+            isCircle: widget.isCircle,
+            hand: widget.hand,
+            secondsMarker: widget.secondsMarker,
+            handSize: widget.handSize,
+            start: widget.start,
+            end: widget.end,
+            highlightStart: (widget.end / widget.end.toInt()),
+            highlightEnd: (0 / widget.end.toInt()),
+            eventObservable: eventObservable,
+            fontFamily: widget.fontFamily,
+            widthCircle: widget.widthCircle > 30 ? 30 : widget.widthCircle,
+            inactiveColor: widget.inactiveColor,
+            activeColor: widget.activeColor,
+            width: (widget.width == null
+                ? MediaQuery.of(context).size.width / 2
+                : widget.width < (MediaQuery.of(context).size.width / 2)
+                    ? (MediaQuery.of(context).size.width / 2)
+                    : (widget.width > (MediaQuery.of(context).size.width)
+                        ? MediaQuery.of(context).size.width
+                        : widget.width)))
       ],
     );
   }
-
-
 }
-
-
-
-
